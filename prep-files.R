79d307215b8d8ba660bd3e0bdda9c94afdd11bb2
sebastienQNLT <45787958+sebastienQNLT@users.noreply.github.com>
2022-09-17 09:41:19 +0200

    Add files via upload

--- prep-files.R
+++ prep-files.R
@@ -1,21 +1,20 @@
 library(dplyr)
 
-#full files : https://www.data.gouv.fr/fr/datasets/demandes-de-valeurs-foncieres/
-file_name="./valeursfoncieres-2017.txt"
-df<-read.csv2(file_name,sep='|')
+annee=2021
 
-df %>% 
+#full files : https://www.data.gouv.fr/fr/datasets/demandes-de-valeurs-foncieres/
+file_name=paste0("./valeursfoncieres-",annee,".txt")
+df<-read.csv2(file_name,sep='|') %>% 
+  mutate(ID=paste0(Code.departement,Code.commune,Prefixe.de.section,Section,No.plan,Date.mutation)) %>% 
   filter(Nature.mutation %in% c("Vente"))%>% 
   filter(Type.local %in% c("Maison","Appartement")) %>% 
   filter(Surface.reelle.bati>9) %>% 
   filter(Valeur.fonciere>5000 & Valeur.fonciere<10000000) %>% 
-  select(Type.local,Date.mutation,Code.postal,Code.commune,Commune,Surface.reelle.bati,Valeur.fonciere,Nombre.pieces.principales,Surface.terrain) %>% 
-  distinct %>%  #dedoublonnage 
-  group_by(Type.local,Date.mutation,Code.commune,Code.postal,Commune) %>% 
-  summarise(Surface.reelle.bati=sum(Surface.reelle.bati,na.rm=TRUE),
-            Valeur.fonciere=max(Valeur.fonciere,na.rm=TRUE),
-            nblots=n(),
-            Nombre.pieces.principales=sum(Nombre.pieces.principales,na.rm=TRUE),
-            Surface.terrain=sum(Surface.terrain,na.rm=TRUE)) %>% 
-  ungroup %>% 
-  filter(nblots<=3) %>% write.csv2("valeursfoncieres-2017-prep.txt",row.names = FALSE)
+  distinct
+
+filtre.lots<-df %>% group_by(ID) %>% summarise(nb.lots=n()) %>% filter(nb.lots<=3)
+
+df %>% 
+ select(ID,Type.local,Date.mutation,Code.postal,Code.commune,Commune,Surface.reelle.bati,Valeur.fonciere,Nombre.pieces.principales,Surface.terrain) %>% 
+ inner_join(filtre.lots,by="ID")%>% 
+ write.csv2(paste0("./valeursfoncieres-prep-",annee,".txt"),row.names = FALSE)
